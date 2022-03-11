d72734b48f19b1b6dd065f939c271f5d544b0571
codelint <unotseeme@foxmail.com>
2022-03-11 11:19:49 +0800

    enhance

--- docker/ext/nodejs.sh
+++ docker/ext/nodejs.sh
@@ -1,21 +1,25 @@
 #! /bin/none
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-INSTALL g++-8 gcc cmake
+INSTALL g++ g++-8 gcc cmake
 
-if [ ! -f /root/src/node-v16.14.0.tar.gz ]
+NODE_VERSION=16.14.0
+NODE_NAME="node-v$NODE_VERSION"
+NODE_SRC_FILE="/root/src/$NODE_NAME.tar.gz"
+
+if [ ! -f "$NODE_SRC_FILE" ]
 then
-    axel https://npmmirror.com/mirrors/node/v16.14.0/node-v16.14.0.tar.gz 
+    axel https://npmmirror.com/mirrors/node/v$NODE_VERSION/$NODE_NAME.tar.gz -o "$NODE_SRC_FILE"
 fi
 
 cd /root/src
-tar -xf node-v16.14.0.tar.gz
-cd /root/src/node-v16.14.0
+tar -xf $NODE_NAME.tar.gz
+cd "/root/src/$NODE_NAME"
 
 mkdir /opt/nodejs
 ./configure --prefix=/opt/nodejs
 
-make && make install
+make -j4 && make install
 
 cat >>/etc/bash.bashrc <<"EOF"
 if [ -x "/opt/nodejs/bin/node" ]
