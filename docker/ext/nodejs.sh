08d494d59c947b7c1b5c8351fe99e21e417aa9b4
codelint <unotseeme@foxmail.com>
2022-03-11 12:15:55 +0800

    enhance

--- docker/ext/nodejs.sh
+++ docker/ext/nodejs.sh
@@ -1,11 +1,23 @@
 #! /bin/none
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-INSTALL g++ g++-8 gcc cmake
+INSTALL g++-8 gcc cmake
+
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 100
+update-alternatives --config gcc
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 100
+update-alternatives --config g++
 
 NODE_VERSION=16.14.0
-NODE_NAME="node-v$NODE_VERSION"
-NODE_SRC_FILE="/root/src/$NODE_NAME.tar.gz"
+
+if [ -f "/root/src/nodejs.tar.gz" ]
+then
+    NODE_NAME="nodejs"
+    NODE_SRC_FILE="/root/src/nodejs.tar.gz"
+else
+    NODE_NAME="node-v$NODE_VERSION"
+    NODE_SRC_FILE="/root/src/$NODE_NAME.tar.gz"
+fi
 
 if [ ! -f "$NODE_SRC_FILE" ]
 then
